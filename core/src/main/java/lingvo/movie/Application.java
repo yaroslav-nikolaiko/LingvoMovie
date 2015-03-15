86e2059ab9cc8a8dfca5720191653ba1d1a9d217
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2015-03-15 18:04:36 +0200

    add web module - angular and nodejs

--- core/src/main/java/lingvo/movie/Application.java
+++ core/src/main/java/lingvo/movie/Application.java
@@ -2,12 +2,18 @@ package lingvo.movie;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by yaroslav on 01.03.15.
  */
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
