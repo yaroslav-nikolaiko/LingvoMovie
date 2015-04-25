71a0979ea70ab20ba728bcaf8ad709de704d0447
yaroslav <admin>
2015-04-25 19:11:06 +0300

    rest param

--- core/src/main/java/lingvo/movie/core/dao/UserRepository.java
+++ core/src/main/java/lingvo/movie/core/dao/UserRepository.java
@@ -2,10 +2,11 @@ package lingvo.movie.core.dao;
 
 import lingvo.movie.core.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by yaroslav on 01.03.15.
  */
 public interface UserRepository extends CrudRepository<User, Long>{
-    User findByName(String name);
+    User findByName(@Param("name")String name);
 }
