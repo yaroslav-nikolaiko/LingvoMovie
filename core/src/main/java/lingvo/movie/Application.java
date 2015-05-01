60ffc8a1d1276a83e56d8037de4579b95da0de0b
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2015-05-01 21:02:42 +0300

    security try

--- core/src/main/java/lingvo/movie/Application.java
+++ core/src/main/java/lingvo/movie/Application.java
@@ -3,23 +3,35 @@ package lingvo.movie;
 
 import lingvo.movie.core.dao.UserRepository;
 import lingvo.movie.core.entity.User;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 /**
  * Created by yaroslav on 01.03.15.
@@ -34,7 +46,7 @@ public class Application {
     }
 }
 
-@Configuration
+/*@Configuration
 class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
     @Autowired
     UserRepository userRepository;
@@ -42,7 +54,20 @@ class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
     @Override
     public void init(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService());
+        auth.authenticationProvider(new AbstractUserDetailsAuthenticationProvider() {
+            @Override
+            protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+                System.out.println(userDetails);
+            }
+
+            @Override
+            protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
+                return userDetailsService().loadUserByUsername(username);
+            }
+        });
     }
+
+
 
     @Bean
     UserDetailsService userDetailsService() {
@@ -56,25 +81,47 @@ class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
             else throw new UsernameNotFoundException("could not find the user '" + username + "'");
         };
     }
-}
+}*/
 
-
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/**")
-                .hasRole("USER")
+                .hasRole("USER") //access("authentication.name == 'admin'")
                 .and()
                 .formLogin();
     }
 
-/*    @Override
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService());
+    }
+
+    @Bean
+    UserDetailsService customUserDetailsService() {
+        return username -> {
+            User user = userRepository.findByName(username);
+            String ROLE = "admin".equals(user.getName()) ? "ROLE_ADMIN" : "ROLE_USER";
+            if (user!=null) return new org.springframework.security.core.userdetails.User(
+                    user.getName(),
+                    user.getPassword(),
+                    true, true, true, true,
+                    AuthorityUtils.createAuthorityList(ROLE));
+            else throw new UsernameNotFoundException("could not find the user '" + username + "'");
+        };
+    }
+
+    /*    @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ig
+        web.
     }*/
 }
