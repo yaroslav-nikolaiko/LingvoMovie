6b8acc0f94e63ac687e5013fa1ac027d0facb947
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2015-05-06 03:22:59 +0300

    Enum bases Lookup

--- core/src/main/java/lingvo/movie/core/rest/LookupController.java
+++ core/src/main/java/lingvo/movie/core/rest/LookupController.java
@@ -6,10 +6,14 @@ import lingvo.movie.core.entity.utils.Language;
 import lingvo.movie.core.entity.utils.Level;
 import lingvo.movie.core.entity.utils.TextType;
 import org.hibernate.validator.constraints.NotBlank;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.rest.webmvc.RepositoryLinksResource;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -24,7 +28,8 @@ import static java.util.stream.Collectors.toList;
  */
 @RestController
 @RequestMapping("lookup")
-public class LookupController {
+@ExposesResourceFor(LookupItem.class)
+public class LookupController implements ResourceProcessor<RepositoryLinksResource> {
     @RequestMapping(value = "{lookupName}", method = RequestMethod.GET)
     public List<LookupItem> getItems(@PathVariable @NotBlank String lookupName) {
         switch (lookupName.toUpperCase()) {
@@ -36,20 +41,33 @@ public class LookupController {
         return Collections.emptyList();
     }
 
+/*    @RequestMapping(method = RequestMethod.GET)
+    public HttpEntity<Resources<LookupItem>> getItems(@RequestParam("name") @NotBlank String lookupName) {
+        switch (lookupName.toUpperCase()) {
+            case "CATEGORY" : return getItems(Category.class);
+            case "LANGUAGE" : return getItems(Language.class);
+            case "LEVEL" : return getItems(Level.class);
+            case "TEXTTYPE" : return getItems(TextType.class);
+        }
+        return new HttpEntity<>(new Resources<>(Collections.emptyList()));
+    }*/
+
     List<LookupItem> getItems(Class clazz) {
         return Arrays.asList(clazz.getEnumConstants()).stream()
                 .map(item -> LookupItem.clone((LookupItem)item))
                 .collect(toList());
     }
 
-    //---------------------------------------------------//
-    // ******************** Quick Test *****************//
-    //-------------------------------------------------//
-    public static void main(String[] args) {
-        LookupController controller = new LookupController();
-        for (LookupItem category : controller.getItems("Category")) {
-            System.out.println("Name " + category.getName());
-            System.out.println("Description " + category.getDescription());
-        }
+/*    HttpEntity<Resources<LookupItem>> getItems(Class clazz) {
+        return new HttpEntity<>( new Resources<>(Arrays.asList(clazz.getEnumConstants()).stream()
+                .map(item -> LookupItem.clone((LookupItem)item))
+                .collect(toList())));
+    }*/
+
+    @Override
+    public RepositoryLinksResource process(RepositoryLinksResource resource) {
+        Link lookup = ControllerLinkBuilder.linkTo(LookupController.class).withRel("lookup");
+        resource.add(lookup);
+        return resource;
     }
 }
