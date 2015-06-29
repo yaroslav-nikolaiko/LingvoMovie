861d637427484bdd68525dd737548317fac4a731
Yaroslav Nikolaiko <nikolaiko.yaroslav@gmail.com>
2015-06-30 00:29:08 +0300

    security test (cont)

--- core/src/test/java/lingvo/movie/core/rest/security/AbstractSecurityRestTest.java
+++ core/src/test/java/lingvo/movie/core/rest/security/AbstractSecurityRestTest.java
@@ -4,11 +4,12 @@ import lingvo.movie.core.dao.UserRepository;
 import lingvo.movie.core.rest.AbstractRestTest;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import static lingvo.movie.core.utils.EntityFactory.userWithRoleADMIN;
 import static lingvo.movie.core.utils.EntityFactory.userWithRoleUSER;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
 
 /**
  * Created by yaroslav on 6/28/15.
@@ -16,6 +17,12 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 public class AbstractSecurityRestTest extends AbstractRestTest {
     @Autowired
     UserRepository userRepository;
+
+/*    @Autowired
+    private Filter springSecurityFilterChain;*/
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Before
     public void setup() throws Exception {
@@ -28,6 +35,10 @@ public class AbstractSecurityRestTest extends AbstractRestTest {
         this.mockMvc = webAppContextSetup(webApplicationContext)
                 .apply(springSecurity())
                 .build();
+
+/*        this.mockMvc = webAppContextSetup(webApplicationContext)
+                .addFilters(springSecurityFilterChain)
+                .build();*/
     }
 
 }
--- core/src/test/java/lingvo/movie/core/rest/security/SecurityRestTest.java
+++ core/src/test/java/lingvo/movie/core/rest/security/SecurityRestTest.java
@@ -3,16 +3,21 @@ package lingvo.movie.core.rest.security;
 import org.hamcrest.core.IsNot;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
 import javax.servlet.http.Cookie;
 
 import static lingvo.movie.core.utils.EntityFactory.principalWithRoleUSER;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 
 /**
  * Created by yaroslav on 6/28/15.
@@ -48,21 +53,51 @@ public class SecurityRestTest extends AbstractSecurityRestTest{
     }
 
     @Test
-    @Ignore
     public void userCouldAccessHisDictionariesTest() throws Exception {
         String id = user.getId().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getName());
 
-        MockHttpServletResponse loginResponse = mockMvc.perform(post("/login")
-                .param("username", admin.getName())
-                .param("password", admin.getPassword()))
-                .andReturn().getResponse();
+        mockMvc.perform(get("/users/" + id + "/dictionaries").with(user(userDetails)))
+                .andExpect(status().isOk());
+    }
 
-        Cookie jsessionid = loginResponse.getCookie("JSESSIONID");
+    @Test
+    public void shouldReturnForbiddenWhenUserAccessAnotherUserDictionaryTest() throws Exception {
+        String anotherUserID = admin.getId().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getName());
 
-/*        mockMvc.perform(get("/users/" + id + "/dictionaries").with(principalWithRoleUSER()))
-                .andExpect(status().isOk()); */
-        mockMvc.perform(get("/users/" + id + "/dictionaries")
-                .cookie(jsessionid))
+        mockMvc.perform(get("/users/" + anotherUserID + "/dictionaries").with(user(userDetails)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void adminCouldAccessAnotherUserDictionaryTest() throws Exception {
+        String anotherUserID = user.getId().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(admin.getName());
+
+        mockMvc.perform(get("/users/" + anotherUserID + "/dictionaries").with(user(userDetails)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void lookupAvailableForNonAuthorizedUserTest() throws Exception {
+        mockMvc.perform(get("/public/lookup").param("name", "language"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturnForbiddenWhenUserSearchForUserByNameTest() throws Exception {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getName());
+
+        mockMvc.perform(get("/users/search/findByName").param("name", user.getName()).with(user(userDetails)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void adminCouldSearchForUserByNameTest() throws Exception {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(admin.getName());
+
+        mockMvc.perform(get("/users/search/findByName").param("name",user.getName()).with(user(userDetails)))
                 .andExpect(status().isOk());
     }
 }
